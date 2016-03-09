24fd830cf0c295410e410be339aa4d525799a199
Carson Cole <carson.cole@nuku.com>
2016-03-08 16:11:43 -0800

    added debugging

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,6 +3,8 @@ class NotificationMailer < ActionMailer::Base
 
   def create(notification_id)
     @notification= Notification.find(notification_id)
+    Rails.logger.fatal "ENV['GMAIL_PASSWORD'] = #{ENV['GMAIL_PASSWORD']}"
+    Rails.logger.fatal "ENV['GMAIL_USERNAME'] = #{ENV['GMAIL_USERNAME']}"
     mail(to: @notification.email, subject: "You are signed up for job notifications")
   end
 
