27c65cfcf2d608504e551d1990a271af2d2d09fb
Carson Cole <carson.cole@nuku.com>
2016-03-09 11:39:33 -0800

    added email validation

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,7 @@
 class Notification < ActiveRecord::Base
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   before_save :downcase_email!
 
