1be5bf225043cad683f46e0b44f1c50a45b865ba
Carson Cole <carson.cole@nuku.com>
2016-03-08 17:37:14 -0800

    minor fixes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,11 @@
 class Notification < ActiveRecord::Base
   validates :email, presence: true
+
+  before_save :downcase_email!
+
+  private
+
+  def downcase_email!
+    self.email = email.downcase
+  end
 end
