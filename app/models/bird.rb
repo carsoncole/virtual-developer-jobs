b5c7bcce16838ce8d387cd7ad297df5d7625ba92
Carson Cole <carson.cole@nuku.com>
2016-03-29 15:14:27 -0700

    fix

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,21 +1,16 @@
 class Bird
 
   def ruby_on_rails
-    set_twitter_connection
-    jobs = Job.tagged_with("Ruby on Rails").where("published_at > ?", Date.today - 30.days)
-    message = "Our #{jobs.size} current remote Ruby on Rails jobs https://virtualdeveloperjobs.com/?tag=Ruby%20on%20Rails #RoR #RemoteJob"
-    tweet = client.update(message)
-  end
-
-
-  private
-
-  def set_twitter_connection
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = AUTHENTICATION["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = AUTHENTICATION["TWITTER_CONSUMER_SECRET"]
       config.access_token        = AUTHENTICATION["TWITTER_ACCESS_TOKEN"]
       config.access_token_secret = AUTHENTICATION["TWITTER_ACCESS_TOKEN_SECRET"]
     end
+
+    jobs = Job.tagged_with("Ruby on Rails").where("published_at > ?", Date.today - 30.days)
+    message = "Our #{jobs.size} current remote Ruby on Rails jobs https://virtualdeveloperjobs.com/?tag=Ruby%20on%20Rails #RoR #RemoteJob"
+    tweet = client.update(message)
   end
+  
 end
