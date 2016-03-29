58d0ae72adc4da8001edccf94ba26f4bd4984b3b
Carson Cole <carson.cole@nuku.com>
2016-03-29 15:08:30 -0700

    added Ror tweet

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -62,6 +62,11 @@ class JobsController < ApplicationController
     end
   end
 
+  def ruby_on_rails
+    
+  end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_job
