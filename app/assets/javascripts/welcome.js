3fb9abc11fff7c9f12f4a366b980166b6168e247
Carson Cole <carson.cole@nuku.com>
2016-03-30 19:23:23 -0700

    styling for welcome

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,9 @@
-$( document ).ready(function() {
-  $( ".job.card" ).click(function() {
-     $(this).find(".description").slideToggle( "slow", function() {
-    });
+
+$(function() {
+  $( ".job.card" ).click(function(){
+    $(this).toggleClass('fullscreen', 1000);
+    $('body').toggleClass('black',1000);
+    $('.close-icon').toggle();
+    $('.description').toggle();
   });
 });
