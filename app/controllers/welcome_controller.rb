5081e268682d6d4d47eed2828732dc76e946ce3d
Carson Cole <carson.cole@nuku.com>
2016-03-31 14:09:56 -0700

    added selection functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      if params[:tag] == 'front-end-jobs'
+    if params[:tags]
+      tags = params[:tags].split('_')
+      if tags.include? 'front-end-jobs'
         @jobs = Job.tagged_with(["Angular", "Angular.js", "Javascript", "Node.js", "Ember", "Ember.js", "jQuery", "Backbone.js", "Backbone", "React.js"], :any => true).where("published_at > ?", Date.today - 30.days)
       else
-        @jobs = Job.tagged_with(params[:tag]).where('published_at NOT NULL').order('published_at DESC')
+        @jobs = Job.tagged_with(tags).where('published_at NOT NULL').order('published_at DESC')
       end
     else
       @jobs = Job.where('published_at NOT NULL').order('published_at DESC')
