5828310cff6f80c735836e3bdd0006073a6d1de9
Carson Cole <carson.cole@nuku.com>
2016-03-31 22:50:01 -0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module Virtualdeveloperjobs
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Pacific Time (US & Canada)'
 
-    config.paperclip_defaults = { storage: :fog, fog_credentials: { provider: "Local", local_root: "#{Rails.root}/public"}, fog_directory: "", fog_host: "localhost"}
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
