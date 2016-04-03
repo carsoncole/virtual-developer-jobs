85227b2342e1f77e9955d462eaefe6efe632ef70
Carson Cole <carson.cole@nuku.com>
2016-04-03 10:53:28 -0700

    fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,14 +5,18 @@ class WelcomeController < ApplicationController
       if tags.include? 'front-end-jobs'
         @jobs = Job.tagged_with(["Angular", "Angular.js", "Javascript", "Node.js", "Ember", "Ember.js", "jQuery", "Backbone.js", "Backbone", "React.js"], :any => true).where("published_at > ?", Date.today - 30.days)
       else
-        @jobs = Job.tagged_with(tags).where('published_at NOT NULL')
+        @jobs = Job.tagged_with(tags).published
       end
     else
-      @jobs = Job.where('published_at NOT NULL')
+      @jobs = Job.published
     end
 
     @job_banner = @jobs.order('rank DESC, published_at DESC').limit(1).first
-    @jobs = @jobs.order('published_at DESC').where.not(id: @job_banner.id)
+    
+    if @job_banner
+      @jobs = @jobs.order('published_at DESC').where.not(id: @job_banner.id)
+    end
+
 
     @all_tags = ActsAsTaggableOn::Tag.most_used(12).map {|t| t.name }.delete_if{|x| ['CSS', 'HTML', 'HTML5'].include? x }
     @param_tags = params[:tags].split("_") if params[:tags]
